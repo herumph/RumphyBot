996ce574390fcc7e278d7e82bc1175254f12e930
herumph <sfdavis0@gmail.com>
2016-12-03 21:44:52 -0500

    Add files via upload
    Updated code that includes tracking for comments within linked threads and comments with the lowest and highest scoring.

--- rumphybot.py
+++ rumphybot.py
@@ -1,11 +1,12 @@
 #!/Users/sfdavis/anaconda3/bin/python
 
-#Bot to save linked threads before they are deleted or removed by a mod. Last updated 12/01/16.
+#Bot to save linked threads before they are deleted or removed by a mod. Last updated 12/03/16.
 
 import praw
 import os.path
 import re
 from config_bot import *
+from datetime import *
 import random
 
 #Reddit stuff
@@ -29,8 +30,8 @@ def write_out(input_string,input_array):
 	return
 
 #Fetching arrays
-personal_messages = get_array("personal_messages")
 already_done = get_array("already_done")
+post = get_array("post")
 
 print("\n * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n")
 
@@ -38,6 +39,8 @@ print("\n * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * \n
 for submission in subreddit.get_new(limit=2):
 	#Link posts.
 	if(not submission.is_self and submission.id not in already_done):
+		already_done.append(submission.id)
+		write_out("already_done",already_done)
 		#Getting if url is a submission or comment and acting accordingly.
 		link = r.get_submission(submission_id = submission.url)
 		link_len = submission.url.split('/')
@@ -48,18 +51,18 @@ for submission in subreddit.get_new(limit=2):
 			url_text = "\n \n **Text** \n \n"+link.selftext
 			author_text = "\n \n- "+str(link.author)
 			sim_text = "\n \n +/u/user_Simulator "+str(link.author)
-			already_done.append(submission.id)
-			write_out("already_done",already_done)
-			submission.add_comment(url_tit+url_text+author_text+sim_text)
+			new_comment = submission.add_comment(url_tit+url_text+author_text+sim_text)
+			post.append(new_comment.id)
+			post.append(link.id)
 		else:
 			text = link.comments[0].body
 			author = link.comments[0].author
 			sim_text = "\n \n +/u/user_Simulator "+str(author)
-			already_done.append(submission.id)
-			write_out("already_done",already_done)
-			submission.add_comment("**Link comment text** \n \n"+text+"\n \n"+"- "+str(author)+sim_text)
+			new_comment = submission.add_comment("**Link comment text** \n \n"+text+"\n \n"+"- "+str(author)+sim_text)
 	#Self posts.
 	elif(submission.is_self and submission.id not in already_done):
+		already_done.append(submission.id)
+		write_out("already_done",already_done)
 		text = submission.selftext
 		reddit_index = [n for n in range(len(text)) if text.find("reddit.com",n) == n]
 		links = []
@@ -69,8 +72,6 @@ for submission in subreddit.get_new(limit=2):
 			#Getting rid of fancy format parenthesis.
 			temp = temp[0].split(')',1)
 			links.append(temp[0])
-		already_done.append(submission.id)
-		write_out("already_done",already_done)
 		#Only taking valid links.
 		for i in links:
 			try:
@@ -84,12 +85,14 @@ for submission in subreddit.get_new(limit=2):
 					url_text = "\n \n **Text** \n \n"+link.selftext
 					author_text = "\n \n- "+str(link.author)
 					sim_text = "\n \n +/u/user_Simulator "+str(link.author)
-					submission.add_comment(url_tit+url_text+author_text+sim_text)
+					new_comment = submission.add_comment(url_tit+url_text+author_text+sim_text)
+					post.append(new_comment.id)
+					post.append(link.id)
 				else:
 					text = link.comments[0].body
 					author = link.comments[0].author
 					sim_text = "\n \n +/u/user_Simulator "+str(author)
-					submission.add_comment("**Linked comment text** \n \n"+text+"\n \n"+"- "+str(author)+sim_text)
+					new_comment = submission.add_comment("**Linked comment text** \n \n"+text+"\n \n"+"- "+str(author)+sim_text)
 			except:
 				pass
 
@@ -97,20 +100,21 @@ for submission in subreddit.get_new(limit=2):
 for comment in subreddit_comments:
 	#Replying to thanks messages.
 	if(comment.body.lower().count("thanks rumphybot") and comment.id not in already_done):
+		already_done.append(comment.id)
+		write_out("already_done",already_done)
+		personal_messages = get_array("personal_messages")
 		if(personal_messages.count(str(comment.author))):
 			#There's probably a better way to do this but it works well enough.
 			for i in range(0,len(personal_messages)-1):
 				if(personal_messages[i] == str(comment.author)):
 					comment.reply(personal_messages[i+1])
-			already_done.append(comment.id)
-			write_out("already_done",already_done)
 		else:
 			comment.reply("No problem <3")
-			already_done.append(comment.id)
-			write_out("already_done",already_done)
 
 	#Replying to save requests. Basically the same as going through self posts.
 	if(comment.body.lower().count("rumphybot save") and comment.id not in already_done):
+		already_done.append(comment.id)
+		write_out("already_done",already_done)
 		text = comment.body
 		reddit_index = [n for n in range(len(text)) if text.find("reddit.com",n) == n]
 		links = []
@@ -120,8 +124,6 @@ for comment in subreddit_comments:
 			#Getting rid of fancy format parenthesis.
 			temp = temp[0].split(')',1)
 			links.append(temp[0])
-		already_done.append(comment.id)
-		write_out("already_done",already_done)
 		#Only taking valid links.
 		for i in links:
 			try:
@@ -135,21 +137,23 @@ for comment in subreddit_comments:
 					url_text = "\n \n **Text** \n \n"+link.selftext
 					author_text = "\n \n- "+str(link.author)
 					sim_text = "\n \n +/u/user_Simulator "+str(link.author)
-					comment.reply(url_tit+url_text+author_text+sim_text)
+					new_comment = comment.reply(url_tit+url_text+author_text+sim_text)
+					post.append(new_comment.id)
+					post.append(link.id)
 				else:
 					text = link.comments[0].body
 					author = link.comments[0].author
 					sim_text = "\n \n +/u/user_Simulator "+str(author)
-					comment.reply("**Linked comment text** \n \n"+text+"\n \n"+"- "+str(author)+sim_text)
+					new_comment = comment.reply("**Linked comment text** \n \n"+text+"\n \n"+"- "+str(author)+sim_text)
 			except:
 				pass
 
 	#Responding to what do with bad advice.
 	if(comment.body.lower().count("rumphybot what do") and comment.id not in already_done):
-		what_do = get_array("what_do")
-		message = random.choice(what_do)
 		already_done.append(comment.id)
 		write_out("already_done",already_done)
+		what_do = get_array("what_do")
+		message = random.choice(what_do)
 		comment.reply(message)
 
 	#Responding to shin splints.
@@ -160,11 +164,10 @@ for comment in subreddit_comments:
 
 	#Stalking Shoes.
 	if(str(comment.author) == "YourShoesUntied" and comment.id not in already_done):
-		hitler_speech = get_array("hitler_speech")
-		print(len(hitler_speech))
-		message = random.choice(hitler_speech)
 		already_done.append(comment.id)
 		write_out("already_done",already_done)
+		hitler_speech = get_array("hitler_speech")
+		message = random.choice(hitler_speech)
 		comment.reply(message)
 
 	#Responding to not nice messages.
@@ -172,3 +175,34 @@ for comment in subreddit_comments:
 		already_done.append(comment.id)
 		write_out("already_done",already_done)
 		comment.reply("http://imgur.com/08gDI9E")
+
+#Going to edit a post made on a thread for an hour after it's posted
+temp = []
+for i in range(0,len(post)-1,2):
+	post_id = "t1_"+post[i]
+	link = r.get_info(thing_id = post_id)
+	submission = r.get_submission(submission_id = post[i+1])
+	if(datetime.utcnow() - datetime.fromtimestamp(link.created_utc) > timedelta(hours=6)):
+		temp.append(i)
+	else:
+		if(link.body.count("---")):
+			old_comment = link.body
+			dash_index = [n for n in range(len(old_comment)) if old_comment.find("---",n) == n]
+			message = old_comment[0:dash_index[0]]
+			comments = sorted(praw.helpers.flatten_tree(submission.comments), key=lambda x: x.score)
+			if(len(comments) > 4):
+				comment_text = "\n \n---\n \n**Best comment from thread:** \n \n"+comments[-1].body+" [source]("+comments[-1].permalink+")\n \n**Worst Comment from thread:** \n \n"+comments[0].body+" [source]("+comments[0].permalink+")"
+				link.edit(message+comment_text)
+		else:
+			message = link.body
+			comments = sorted(praw.helpers.flatten_tree(submission.comments), key=lambda x: x.score)
+			if(len(comments) > 4):
+				comment_text = "\n \n---\n \n**Best comment from thread:** \n \n"+comments[-1].body+" [source]("+comments[-1].permalink+")\n \n**Worst Comment from thread:** \n \n"+comments[0].body+" [source]("+comments[0].permalink+")"
+				link.edit(message+comment_text)
+
+#Getting rid of old posts to not edit
+for i in range(0,len(temp)-1,2):
+	del post[temp[i]]
+	del post[temp[i+1]]
+
+write_out("post",post)
